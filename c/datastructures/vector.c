408fdd4509e2ac843d6b32fc4df9c5a3ad82100e
Sunday Alexander <alexander@nebula.io>
2025-03-18 15:35:34 +0100

    Added  C implementation of vector.

---
+++ c/datastructures/vector.c
@@ -0,0 +1,14 @@
+/**
+An implementation of a Vector mutable array with automatic resizing data structure.
+Implementation approach can be found here: https://github.com/sundayalexander/coding-interview-university#data-structures
+This data structure is for learning purpose only, an optimized implementation is available for production use in the C community.
+**/
+#include <stdlib.h>
+struct {
+
+}
+
+int main(void){
+
+return 0
+}
